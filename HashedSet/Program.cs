8fd2121cbb02060848bbc6d8205cb8c877bfdd69
danielchoike2 <89948222+danielchoike2@users.noreply.github.com>
2023-03-19 12:47:06 -0400

    Add project files.

---
+++ HashedSet/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace WorkingWithHashSets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> videoGames = new HashSet<string>();
+            videoGames.Add("Resident Evil");
+            videoGames.Add("Resident Evil 2");
+            videoGames.Add("Resident Evil 3");
+            videoGames.Add("Resident Evil 4");
+            videoGames.Add("Dead Space");
+
+            HashSet<string> videoGamesRemakes = new HashSet<string>();
+            videoGamesRemakes.Add("Resident Evil Remastered");
+            videoGamesRemakes.Add("Resident Evil 2: Remastered");
+            videoGamesRemakes.Add("Resident Evil 3: Remastered");
+            videoGamesRemakes.Add("Resident Evil 4 : Remastered");
+            videoGamesRemakes.Add("Dead Space Remastered");
+
+            HashSet<string> videoGameSequels = new HashSet<string>();
+            videoGameSequels.Add("Resident Evil 2 ");
+            videoGameSequels.Add("Resident Evil 3");
+            videoGameSequels.Add("The Last of Us 2");
+            videoGameSequels.Add("Dark Cloud 2");
+            videoGameSequels.Add("Final Fantasy 13");
+
+            Console.WriteLine("Combined list of older games with remastered versions");
+            videoGames.UnionWith(videoGamesRemakes);
+            foreach (string videogame in videoGames)
+            {
+                Console.WriteLine(videogame);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Video games that are in both lists");
+            HashSet<string> bothLists = new HashSet<string>();
+            bothLists = videoGames;
+            bothLists.IntersectWith(videoGamesRemakes);
+            foreach (string language in bothLists)
+            {
+                Console.WriteLine(language);
+            }
+        }
+    }
+}
